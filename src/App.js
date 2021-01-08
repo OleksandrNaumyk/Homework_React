e08f3ddaa86aeade57926417eed4772cbc694549
OleksandrNaumyk <naumyk@ukr.net>
2021-01-08 21:12:00 +0200

    lesson7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useReducer} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_TODO': {
+            return action.payload;
+        }
+        case 'CHANGE_TODO_STATUS': {
+            return {...state,
+            completed: !state.completed
+            };
+        }
+        case 'CHANGE_TODO_TITLE': {
+            return {
+                ...state,
+                title: action.payload
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+};
+
+const initialState = {
+    userId: null,
+    id: null,
+    title: '',
+    completed: false
+};
+
+export default function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+//    const [user, setUser] = useState({name: "a", age: 30});
+    const [counter, setCounter] = useState(1);
+//    const [todo, setTodo] = useState();
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
+            .then(response => response.json())
+            .then(json => {
+                dispatch({type: 'SET_TODO', payload: json})
+                console.log(json);
+            })
+    }, [counter]);
+
+    const onClickHandler = () => setCounter((prev) => prev + 1);
+    const onStatusChange = () => dispatch({type: 'CHANGE_TODO_STATUS'})
+    const onTitleChange = () => dispatch({type: 'CHANGE_TODO_TITLE', payload: Math.random()})
+
+    return (
+            <div className={App}>
+                <button onClick={onClickHandler}>inc</button>
+                <button onClick={onStatusChange}>change todo status</button>
+                <button onClick={onTitleChange}>change todo title</button>
+                <h1>Counter value: {counter}</h1>
+                {
+                    !!state && (
+                        <>
+                            <h2>{state.id}</h2>
+                            <h2>{state.title}</h2>
+                            <h2>{state.completed.toString()}</h2>
+                        </>
+                    )
+                }
+            </div>
+        );
 }
 
-export default App;
